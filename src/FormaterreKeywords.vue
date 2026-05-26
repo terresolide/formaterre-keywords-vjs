fe8ef044df08210cd42ed0273816aacd129887b7
epointal <pointal@ipgp.fr>
2026-05-26 09:22:03 +0200

    get thesaurus list  Geonetwork

--- src/FormaterreKeywords.vue
+++ src/FormaterreKeywords.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Coucou {{ lang }}</h1>
+    <span>
+        <h1>Coucou {{ lang }}</h1>
+        {{ vocabularies }}
+        <div v-for="th in vocabularies">{{ th.dname }}- {{ th.filename }} - {{ th.title }}</div>
+
+    </span>
 </template>
 <script>
 export default {
@@ -8,6 +13,25 @@ export default {
         lang: {
             type: String,
             default: 'fr'
+        },
+        geonetwork: {
+            type: String,
+            default: 'https://catalogue-terresolide.ipgp.fr/geonetwork'
+        }
+    },
+    data () {
+        return {
+            vocabularies: []
+        }
+    },
+    mounted () {
+        this.getVocabularies()
+    },
+    methods: {
+        getVocabularies () {
+            fetch(this.geonetwork + '/srv/fre/thesaurus?_content_type=json')
+            .then(resp => resp.json())
+            .then(json => this.vocabularies = json[0])
         }
     }
 }
